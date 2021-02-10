25b2a28a78f550307a0a8f72c017b58d858f972f
Kevin Pennekamp <github@kevtiq.co>
2021-02-10 21:13:57 +0100

    initial environmental setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest',
+  },
+  setupFiles: ['jest-localstorage-mock'],
+};
